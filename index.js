0fa1f9cf224eee0d7637c3194ca96c37b7ef1a05
Prisca C <prisca.castanier@gmail.com>
2022-11-22 12:02:56 +0100

    feat: you can now read on data directory and get each category and/or files

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
 //create a server object:
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(); //end the response
+
+  let r = url.parse(req.url, true).pathname;
+  let q = url.parse(req.url, true).query;
+
+  if (r === '/category') {
+    if(!q.category) {
+      let category = []
+      fs.readdir('./data', function (err, files) {
+        if (err) {
+          res.end('Unable to read directory:' + err);
+        } else {
+          files.forEach(function (file) {
+            category.push(file)
+          });
+          res.end(JSON.stringify(category));
+        }
+      })
+    } else {
+      fs.readdir(`./data/${q.category}`, function(err, files) {
+        if (err) {
+          res.end('Unable to scan directory: ' + err);
+        }
+        res.end(JSON.stringify(files));
+      })
+    }
+  }
 }).listen(8080); //the server object listens on port 8080
